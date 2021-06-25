a3240b3aada86476a2e7fae6cbe571d15c26574f
myoneflag <arre0611@hotmail.com>
2021-06-25 21:26:44 +0200

    init

---
+++ assets/js/custom.js
@@ -0,0 +1,26 @@
+/* Custom JS */
+
+var transparent = true;
+var big_image;
+
+var transparentDemo = true;
+var fixedTop = false;
+
+var navbar_initialized,
+  backgroundOrange = false,
+  toggle_initialized = false;
+
+var $datepicker = $('.datepicker');
+var $collapse = $('.navbar .collapse');
+var $html = $('html');
+var $tagsinput = $('.tagsinput');
+
+(function() {
+})();
+
+$(document).ready(function() {
+
+});
+
+$(document).on('click', '.card-rotate .btn-rotate', function() {
+});
